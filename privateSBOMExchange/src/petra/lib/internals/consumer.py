b5f80cc189e52edbc3711e1c635f0d94d38e7440
Eman Abu Ishgair <eabuishg@purdue.edu>
2025-12-06 22:04:09 -0500

    fix nits

--- privateSBOMExchange/src/petra/lib/internals/consumer.py
+++ privateSBOMExchange/src/petra/lib/internals/consumer.py
@@ -11,7 +11,7 @@ class Consumer:
         self.kms_url = kms_conf.get_kms_service_url()
         self.sw_artifact = sw_artifact
         self.redacted_sbom_tree = redacted_sbom_tree
-        self.key_expiry:int=0
+        self.key_expiry:int = 0
         self.cpabe_sk = self.enroll_decryption_key()
         self.decrypted_sbom_tree = None
 
@@ -19,7 +19,7 @@ class Consumer:
         response = requests.post(f"{self.kms_url}/enroll")
         if response.status_code != 200:
             raise Exception(f"Failed to get cpabe secret key: {response.text}")
-        self.key_expiry=response.json().get("expires")
+        self.key_expiry = response.json().get("expires")
         return response.json().get("cpabe_pk")
 
     def decrypt_sbom(self):
